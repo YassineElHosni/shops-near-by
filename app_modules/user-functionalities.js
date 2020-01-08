3d826eb003f3b83a7d712d45eb2a2004365b2fd1
YassineH <hosni.yassine.yh@gmail.com>
2020-01-08 18:34:11 +0100

    Added sign in related functions
    The following functions were added:
    - User.copy user : to copy a user object into the calling object.
    - User.to required object : to return the calling object to an other.

--- app_modules/user-functionalities.js
+++ app_modules/user-functionalities.js
@@ -10,6 +10,21 @@ function User(email, password){
    this.password = password;
 
    // TODO: crypt user password
+   
+   this.copyUser = async(user) => {
+      this.id = user.id;
+      this.email = user.email;
+      this.password = user.password;
+      //await this.cryptUserPassword();
+
+   }
+   this.toRequiredObject = function(){
+      return {
+         id: this.id,
+         email: this.email,
+         password: this.password
+      };
+   }
 }
 
 function signup(request, response){
@@ -20,4 +35,13 @@ function signup(request, response){
 	db_request.insertUser(user, response);
 };
 
+function signin(request, response){
+	var user = new User(
+		request.body.email,
+		request.body.password
+	);
+	db_request.selectUserByEmailPw(user, request, response);
+};
+
 exports.signup = signup;
+exports.signin = signin;
