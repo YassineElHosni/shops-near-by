89d253e7a2024fbdbfe5080595bc36c5cdf94cd2
YassineH <hosni.yassine.yh@gmail.com>
2020-01-08 17:12:15 +0100

    Added sign up required methods
    The following methods were added:
    - clear input : to clear the form inputs after sign up.
    - display message : to switch between error and succes messages resived from the web service.
    - sign up : to read and send the email and password of the new user, and wait for the response.

--- public/js/main-vue.js
+++ public/js/main-vue.js
@@ -1,6 +1,50 @@
 window.onload = init;
 function init() {
-    Vue.use(VueMaterial.default);
-    new Vue({
-    });
+	Vue.use(VueMaterial.default);
+	new Vue({
+		el: "#main",
+		data: {
+			message: "empty",
+			email:"",
+			password:"",
+			new_email:"",
+			new_password:"",
+			signup_url: "/signup"
+		},
+		methods: {
+			clearInput(){
+				this.new_email = "";
+				this.new_password = "";
+			},
+			displayMessage(message, error=false){
+				this.clearInput();
+				if(error){
+					document.querySelector(".error-msg").style.display="block";
+					document.querySelector(".error-msg").innerHTML = message;
+					document.querySelector(".success-msg").style.display="none";
+				}else{
+					document.querySelector(".success-msg").style.display="block";
+					document.querySelector(".success-msg").innerHTML = message;
+					document.querySelector(".error-msg").style.display="none";
+				}
+			},
+			signup(){
+				console.log("signing up as "+this.new_email+" "+this.new_password);
+				let form_data = new FormData();
+				form_data.append("email", this.new_email);
+				form_data.append("password",  this.new_password);
+
+				fetch(this.signup_url, {method:"POST", body:form_data}).then(response => {
+					return response.json();
+				}).then(response => {
+					if(response.error){
+						this.displayMessage(response.message, true);
+					}else {
+						this.displayMessage(response.message);
+						// TODO: display sign in form
+					}
+				});
+			}
+		}
+	});
 };
