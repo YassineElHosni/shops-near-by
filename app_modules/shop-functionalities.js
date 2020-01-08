6cab6a0005f661fce4b6b221a13b22232e4d5067
YassineH <hosni.yassine.yh@gmail.com>
2020-01-09 00:20:55 +0100

    Added two functions, 'near by shops' and 'prefered shops'
    Both functions call out to the 'request current location from user' and 'delete disliked shops added two hours ago' functions and they return the list of shops that their names define.

--- app_modules/shop-functionalities.js
+++ app_modules/shop-functionalities.js
@@ -12,12 +12,35 @@ function likeShop(request, response){
 	);
 };
 function dislikeShop(request, response){
+	db_request.insertDislikedShopByUser(
+		parseInt(request.body.shop_id),
+		request.session.user.id,
+		response
+	);
+};
+function nearByShops(request, response){
+	var user =  request.session.user;
 
-   db_request.insertDislikedShopByUser(
-      parseInt(request.body.shop_id),
-      request.session.user.id,
-      response
-   );
+	file_log("routes.user." + arguments.callee.name, "requeting user position");
+	user.location = db_request.requestCurrentLocationFromUser();
+
+	db_request.deleteDislikedShopAddedTwoHourAgo(user.id);
+
+	file_log("routes.user." + arguments.callee.name, "requesting near by shops");
+	db_request.selectShopsNearMe(user, response);
+};
+function preferedShops(request, response){
+	var user =  request.session.user;
+
+	file_log("routes.user." + arguments.callee.name, "requeting user position");
+	user.location = db_request.requestCurrentLocationFromUser();
+
+	db_request.deleteDislikedShopAddedTwoHourAgo(user.id);
+
+	file_log("routes.user." + arguments.callee.name, "requested shops for user n° "+user.id+".");
+	db_request.selectPreferedShops(user, response);  
 };
 exports.likeShop = likeShop;
 exports.dislikeShop = dislikeShop;
+exports.nearByShops = nearByShops;
+exports.preferedShops = preferedShops;
