1666a15bd568668c3640ca99c8801619caaa7a65
YassineH <hosni.yassine.yh@gmail.com>
2020-01-08 17:21:06 +0100

    Update db-data-access.js
    Added a new database request for a new user insertion using the 'queries' module that contains all sql queries.

--- app_modules/db-data-access.js
+++ app_modules/db-data-access.js
@@ -2,3 +2,21 @@
 * Module dependencies.
 */
 var queries = require('./queries');
+
+function insertUser(user, response){
+	var callee_name = arguments.callee.name;
+	db.query(queries.get.SQL_INSERT_USER,
+		[
+			user.email,
+			user.password
+		],
+	function(error, result) {
+		if(error) throw error;
+		file_log("routes.user." + callee_name, "user account created succesfully");
+		response.send(JSON.stringify({
+			error: false,
+			message: "User account created succesfully!"
+		}));
+	});
+}
+exports.insertUser = insertUser;
