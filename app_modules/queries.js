a7646a717b4c68fe1754d738e02996afd8e418dc
YassineH <hosni.yassine.yh@gmail.com>
2020-01-08 20:09:49 +0100

    Added one new queries to select near by shops

--- app_modules/queries.js
+++ app_modules/queries.js
@@ -4,10 +4,16 @@ function db_tables(tables){
 }
 
 const db_shops = new db_tables({
-	user: "user"
+	user: "user",
+	shops: "shops"
 });
 
 exports.get = {
 	SQL_INSERT_USER: "INSERT INTO `" + db_shops.tables.user + "`(`email`,`password`) VALUES (?, ?);",
-	SQL_SELECT_USER_BY_EMAIL_PW: "SELECT * FROM `" + db_shops.tables.user + "` WHERE `email` = ? and password = ?;"
+	SQL_SELECT_USER_BY_EMAIL_PW: "SELECT * FROM `" + db_shops.tables.user + "` WHERE `email` = ? and password = ?;",
+	SQL_SELECT_SHOPS_NEAR_ME: "SELECT id, name, latitude, longitude, "
+								+"SQRT( POW(69.1 * (latitude - ?), 2) + "
+								+"POW(69.1 * (? - longitude) * COS(latitude / 57.3), 2)) * 1609.34 AS distance "
+							+"FROM `" + db_shops.tables.shops + "` "
+							+"HAVING distance < 1000 ORDER BY distance;"
 };
