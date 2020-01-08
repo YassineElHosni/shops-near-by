601cd654b42af41ceb3aeec1c691df660605415a
YassineH <hosni.yassine.yh@gmail.com>
2020-01-08 20:13:37 +0100

    Added new table 'shops' and Updated 'user' table
    Added the following:
    - 'user' auto_increment starting value.
    - 'shops' table creation script .
    - 'shops' auto_increment starting value.
    - inserted few lines in the 'shops' table.

--- data-base-script.sql
+++ data-base-script.sql
@@ -32,6 +32,34 @@ CREATE TABLE if not exists `user`(
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+ALTER TABLE `user` AUTO_INCREMENT=1000;
+
+--
+-- Table structure for table `shops`
+--
+
+CREATE TABLE if not exists `shops` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `image` varchar(30) NOT NULL,
+  `longitude` varchar(30) NOT NULL,
+  `latitude` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `shops` AUTO_INCREMENT=1000;
+
+--
+-- inserting data for table `shops`
+--
+
+INSERT INTO `shops` (`name`, `image`, `longitude`, `latitude`) VALUES
+('Microland', '', '-6.851237', '34.003058'),
+('Infodis', '', '-6.850231', '34.004120'),
+('Itelsys', '', '-6.851171', '34.003978'),
+('Treshiq', '', '-6.847632', '34.002817'),
+('Isolda shop', '', '-6.848205', '34.004143'),
+('EJ medic', '', '-6.847629', '34.004175'),
+('Learning Design', '', '-6.847473', '34.004272');
 
 
 COMMIT;
