7b2146b71f9fa5552e8365056cd84ab93293199e
YassineH <hosni.yassine.yh@gmail.com>
2020-01-08 18:36:40 +0100

    Added a new querie
    To check if the given user exists

--- app_modules/queries.js
+++ app_modules/queries.js
@@ -8,5 +8,6 @@ const db_shops = new db_tables({
 });
 
 exports.get = {
-	SQL_INSERT_USER: "INSERT INTO `" + db_shops.tables.user + "`(`email`,`password`) VALUES (?, ?);"
+	SQL_INSERT_USER: "INSERT INTO `" + db_shops.tables.user + "`(`email`,`password`) VALUES (?, ?);",
+	SQL_SELECT_USER_BY_EMAIL_PW: "SELECT * FROM `" + db_shops.tables.user + "` WHERE `email` = ? and password = ?;"
 };
