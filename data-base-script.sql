7b47b964dc8deadfe3d56c17b531da88bace9c05
YassineH <hosni.yassine.yh@gmail.com>
2020-01-08 15:58:19 +0100

    Create data-base-script.sql
    Currently this file is set to default, and it requres the user to create a database manualy.

---
+++ data-base-script.sql
@@ -0,0 +1,23 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 08, 2020 at 03:15 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+--
+-- Database: `db_shops`
+--
+
+
+
+
+COMMIT;
