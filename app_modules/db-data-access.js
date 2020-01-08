b2fae580732e6123063e6232f3b0b2dfd67bcfaa
YassineH <hosni.yassine.yh@gmail.com>
2020-01-08 20:01:52 +0100

    Added dashbord near by shops related function
    - request current location for the user : returns the latitude and longitude of the current user.
    - select shops near by me : returns the list of shops near by the current user.
    - select user by email and password (update) : request current location form the user and use the result as parameters for the 'request near by shops list' function.

--- app_modules/db-data-access.js
+++ app_modules/db-data-access.js
@@ -19,36 +19,68 @@ function insertUser(user, response){
 		}));
 	});
 }
+function requestCurrentLocationFromUser(user_id){
+	file_log("routes.user."+arguments.callee.name, "requeting current location from user n° " + user_id);
+	// TODO: request/get latitude and longitude from the user.
+	let latitude = 34.003243399999995
+		,longitude = -6.8487546;
+	file_log("routes.user." + arguments.callee.name, "user position: latitude=" + latitude + ", longitude=" + longitude);
+	return {latitude, longitude};
+}
+function selectShopsNearMe(user, response){
+	var callee_name = arguments.callee.name;
+	file_log("routes.user." + callee_name, "called by user n° " + user.id);
+	db.query(queries.get.SQL_SELECT_SHOPS_NEAR_ME,
+		[
+			user.location.latitude,
+			user.location.longitude,
+			user.id,
+			user.id
+		],
+	function(error, results){
+		if(error) throw error;
+		file_log("routes.user." + callee_name, "succesfully loaded near by shops for user n° " + user.id);
+
+		response.send(JSON.stringify({
+			error: false,
+			data: {
+				user: user,
+				shops: results
+			},
+			message: "Succesfully loaded data!"
+		}));
+	});
+}
 function selectUserByEmailPw(user, request, response){
-   var callee_name = arguments.callee.name;
-   db.query(queries.get.SQL_SELECT_USER_BY_EMAIL_PW,
-      [
-         user.email,
-         user.password
-      ],
-      function(error, results){
-         if(error) throw error;
-         if(results.length == 1){
-            user.copyUser(results[0]);
-            request.session.user = user.toRequiredObject();
+	var callee_name = arguments.callee.name;
+	db.query(queries.get.SQL_SELECT_USER_BY_EMAIL_PW,
+		[
+			user.email,
+			user.password
+		],
+	function(error, results){
+		if(error) throw error;
+		if(results.length == 1){
+			user.copyUser(results[0]);
+			request.session.user = user.toRequiredObject();
 
-            file_log("routes.user." + callee_name, "user n° " + user.id + " succesfully signed in");
+			file_log("routes.user." + callee_name, "user n° " + user.id + " succesfully signed in");
 
-            response.send(JSON.stringify({
-               error: false,
-               message: "Succesfully signed in!"
-            }));
+			user.location = requestCurrentLocationFromUser(user.id);
 
-         }
-         else{
-            file_log("routes.user." + callee_name, "Wrong Credentials", "ERROR");
-            response.send(JSON.stringify({
-               error: true,
-               message: "Wrong credentials!"
-            }));
-         }
-                 
-      });
+			file_log("routes.user." + callee_name, "requesting shops near user n° " + user.id);
+			selectShopsNearMe(user, response);
+		}
+		else{
+			file_log("routes.user." + callee_name, "Wrong Credentials", "ERROR");
+				response.send(JSON.stringify({
+				error: true,
+				message: "Wrong credentials!"
+			}));
+		}
+	});
 }
 exports.insertUser = insertUser;
+exports.requestCurrentLocationFromUser = requestCurrentLocationFromUser;
+exports.selectShopsNearMe = selectShopsNearMe;
 exports.selectUserByEmailPw = selectUserByEmailPw;
